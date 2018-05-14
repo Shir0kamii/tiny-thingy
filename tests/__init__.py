0f5bc3cf0afd213658f1bd94263840fbd2b49ffc
Alexandre Bonnetain <shir0kamii@gmail.com>
2018-05-14 16:42:08 +0000

    Add a test on name discoveries

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,24 @@
+import pytest
+
+from tiny_thingy import Thingy
+
+
 def test_thingy_database(TestThingy, database):
     assert TestThingy.database == database
 
 
 def test_thingy_table(TestThingy, table):
     assert TestThingy.table == table
+
+
+def test_thingy_names(database):
+    class Foo(Thingy):
+        pass
+
+    with pytest.raises(AttributeError):
+        Foo.database
+
+    Foo._database = database
+    assert Foo.database == database
+    assert Foo.table == database.table("foo")
+    assert Foo.table_name == "foo"
--- tiny_thingy/__init__.py
+++ tiny_thingy/__init__.py
@@ -12,3 +12,7 @@ class Thingy(DatabaseThingy):
     @classmethod
     def _get_table(cls, database, table_name):
         return database.table(table_name)
+
+    @classmethod
+    def _get_table_name(cls, table):
+        return table.name
