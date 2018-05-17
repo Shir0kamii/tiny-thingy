8352b55adbea38f40d9e39883bef99061e929588
Alexandre Bonnetain <shir0kamii@gmail.com>
2018-05-17 15:57:00 +0000

    Bump version: 0.0.3 → 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="Tiny-Thingy",
-    version="0.0.3",
+    version="0.1.0",
     url="https://github.com/Shir0kamii/tiny-thingy",
     license="MIT",
     author="Shir0kamii",
