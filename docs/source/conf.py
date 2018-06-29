14b1dc832e7452583e341924f77cd89685454372
Alexandre Bonnetain <shir0kamii@gmail.com>
2018-06-29 00:06:11 +0000

    Use RTFD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,4 +42,4 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
